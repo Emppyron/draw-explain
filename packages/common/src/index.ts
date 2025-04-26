673fa990a3a40d94f4d0598cc04d0dd3f46e9ec4
high-ambitions <amankumar2021ap@gmail.com>
2025-04-26 22:07:31 +0530

    db instantiated

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 
-export const UserZSchema= z.object({
-    username:z.string(),
+export const CreateUserSchema= z.object({
+    username:z.string().min(2).max(30),
     password:z.string(),
     fullName:z.string(),
 })
+
+export const  SigninSchema=z.object({
+    username:z.string().min(2).max(30),
+    password:z.string()
+})
+
+
